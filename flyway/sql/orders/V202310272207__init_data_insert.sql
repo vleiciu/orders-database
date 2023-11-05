501c328f105c1e22392c82905204b73f64ed0c98
vleiciu <volodea.leyciu@gmail.com>
2023-11-05 20:33:43 +0200

    init commit

---
+++ flyway/sql/orders/V202310272207__init_data_insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO ORDERS_OWNER.USERS (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS, ADDRESS, IS_ACTIVE, ROLE)
+VALUES ('Vladimir', 'Leiciu', 'vleiciu', 'volodea.leyciu@gmail.com', '$2a$12$7hhO9BgY9aqV3OCqcyRvd.oRumKHQaEmu8CLDUIu/.4ENEb8fSZaa', 'mock-address', 'Y', 'ADMIN');
+
+INSERT INTO ORDERS_OWNER.USERS (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS, ADDRESS, IS_ACTIVE, ROLE)
+VALUES ('Daria', 'Morskaya', 'dmorskaya', 'daria.morskaya@gmail.com', '$2a$12$7hhO9BgY9aqV3OCqcyRvd.oRumKHQaEmu8CLDUIu/.4ENEb8fSZaa', 'mock-address', 'Y', 'ADMIN');
+
+INSERT INTO ORDERS_OWNER.USERS (FIRST_NAME, LAST_NAME, USERNAME, EMAIL, PASS, ADDRESS, IS_ACTIVE, ROLE)
+VALUES ('Thaimore', 'T', 'thaimore', 'thaimore@mail.ru', '$2a$12$7hhO9BgY9aqV3OCqcyRvd.oRumKHQaEmu8CLDUIu/.4ENEb8fSZaa', 'mock-address', 'Y', 'MANAGER');
+
+COMMIT;
